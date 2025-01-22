2cd28fd7fddaacbb0478b5e13fb5bae6a585c716
Jim <b10430230@outlook.com>
2025-01-22 14:16:38 +0800

    feat:send 後再度轉發send

--- src/main/java/org/example/kafkademo/Controller.java
+++ src/main/java/org/example/kafkademo/Controller.java
@@ -2,6 +2,7 @@ package org.example.kafkademo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,15 @@ public class Controller {
 
 
     @Autowired
-    private KafkaTemplate<Object,Object> kafkaTemplate;
+    private KafkaTemplate<Object, Object> kafkaTemplate;
 
-    @PostMapping(path="/send/foo/{what}")
+    @PostMapping(path = "/send/foos/{what}")
     public void sendFoo(@PathVariable String what) {
-        this.kafkaTemplate.send("topic1", new Foo1(what));
+        this.kafkaTemplate.executeInTransaction(kafkaTemplate -> {
+            StringUtils.commaDelimitedListToSet(what).stream()
+                    .map(s -> new Foo1(s))
+                    .forEach(foo1 -> kafkaTemplate.send("topic2", foo1));
+            return null;
+        });
     }
 }
--- src/main/java/org/example/kafkademo/KafkaDemoApplication.java
+++ src/main/java/org/example/kafkademo/KafkaDemoApplication.java
@@ -3,37 +3,44 @@ package org.example.kafkademo;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.converter.BatchMessagingMessageConverter;
 import org.springframework.kafka.support.converter.JsonMessageConverter;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.util.backoff.FixedBackOff;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class KafkaDemoApplication {
 
+    final static CountDownLatch LATCH = new CountDownLatch(1);
+
     private final Logger log = LoggerFactory.getLogger(KafkaDemoApplication.class);
     private final TaskExecutor taskExecutor = new SimpleAsyncTaskExecutor();
 
-    public static void main(String[] args) {
-        SpringApplication.run(KafkaDemoApplication.class, args).close();
-    }
-
-    @Bean
-    public CommonErrorHandler commonErrorHandler(KafkaTemplate<Object, Object> kafkaTemplate) {
-        return new DefaultErrorHandler(
-                new DeadLetterPublishingRecoverer(kafkaTemplate),
-                new FixedBackOff(1000L, 2));
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext context = SpringApplication.run(KafkaDemoApplication.class, args);
+        LATCH.await();
+        Thread.sleep(5_000);
+        context.close();
     }
 
     @Bean
@@ -41,35 +48,41 @@ public class KafkaDemoApplication {
         return new JsonMessageConverter();
     }
 
-    @KafkaListener(id = "fooGroup", topics = "topic1")
-    public void listen(Foo2 foo) {
-        log.info("Received: {}", foo);
-        if (foo.getFoo().startsWith("fail")) {
-            throw new RuntimeException("failed");
-        }
-        this.taskExecutor.execute(() -> System.out.println("Hit enter to terminate"));
-    }
-
-    @KafkaListener(id="dltGroup",topics = "topic1-dlt")
-    public void dltListen(byte[] in){
-        log.info("Received from DLT : {}", new String(in));
-        this.taskExecutor.execute(() -> System.out.println("Hit enter to terminate"));
+    @Bean
+    public BatchMessagingMessageConverter batchConverter() {
+        return new BatchMessagingMessageConverter(converter());
     }
 
     @Bean
-    public NewTopic topic(){
-        return new NewTopic("topic1",1,(short) 1);
+    public NewTopic topic2() {
+        return TopicBuilder.name("topic2").partitions(1).replicas(1).build();
     }
+
     @Bean
-    public NewTopic dlt(){
-        return new NewTopic("topic1-dlt",1,(short) 1);
+    public NewTopic topic3() {
+        return TopicBuilder.name("topic3").partitions(1).replicas(1).build();
     }
-    @Bean
-    @Profile("default")
-    public ApplicationRunner runner() {
-        return args -> {
-            System.out.println("Hi Enter to terminate...");
-            System.in.read();
-        };
+
+}
+
+@Component
+class Listener {
+    private Logger log = LoggerFactory.getLogger(Listener.class);
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @KafkaListener(id = "fooGroup2", topics = "topic2")
+    public void listen1(List<Foo2> foos) throws IOException {
+        log.info("Received : {}", foos);
+        foos.forEach(f -> kafkaTemplate.send("topic3", f.getFoo().toUpperCase()));
+        log.info("Message sent,hit Enter to commit tx");
+//        System.in.read(); // 要按下後才不會被這function卡死
+    }
+
+    @KafkaListener(id = "fooGroup3", topics = "topic3")
+    public void listen2(List<String> in) {
+        log.info("Received String :{}", in);
+         KafkaDemoApplication.LATCH.countDown();
     }
 }
